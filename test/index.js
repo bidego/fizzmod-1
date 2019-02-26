24a8eb52ea029991a7a14bc02b51cbc7f3f30229
Gonzalo Bide <gonzalo.bide@gmail.com>
2019-02-25 21:58:54 -0300

    primer ej

--- test/index.js
+++ test/index.js
@@ -6,6 +6,11 @@
 
 let numeros = [1,2,3,4]
 //mapCustomizado => representa la funcion que ustedes tendrían que crear
+mapCustomizado = (input, callback) => {
+	let output = [];
+	input.forEach( (v,i) => output.push(callback(v,i)));
+	return output;
+}
 mapCustomizado(numeros,numero=>numero+1) //[2,3,4,5]
 mapCustomizado(numeros,(numero,indice)=>numero+indice) //[1,3,5,7]
 mapCustomizado(numeros,numero=>{}) //[undefined,undefined,undefined,undefined]
@@ -13,6 +18,12 @@ mapCustomizado(numeros,numero=>{}) //[undefined,undefined,undefined,undefined]
 /**
  * 4) Modificar el prototipo de la funcion constructora Array para que admita como nuevo método la funcion customizada del paso anterior para que cumpla los siguientes tests :
  */
+Array.prototype.mapCustomizado = function(input, callback) {
+	let output = [];
+	input.forEach( (v,i) => output.push(callback(v,i)));
+	return output;
+}
+
 numeros.mapCustomizado(numero=>numero+1) //[2,3,4,5]
 numeros.mapCustomizado((numero,indice)=>numero+indice) //[1,3,5,7]
 numeros.mapCustomizado(numero=>{}) //[undefined,undefined,undefined,undefined]
@@ -25,11 +36,11 @@ numeros.hasOwnProperty("mapCustomizado") //false
 
 let miembros = { pedro : 35 , ana : 18 , carlos : 43 , juan : 21 , maria : 29 , angela : 31 , jose : 23 , mariana : 41 , eugenio : 19 }
 
+Object.keys(miembros).sort().forEach( k => (miembros[k] < 25 || miembros[k] > 40) ? mar.push(k) : cen.push(k))
 /**
  * 4) Crear un fork de este repositorio en sus propias cuentas
  * 5) Clonar el fork obtenido
  * 6) Encontrar todos los errores en el siguiente programa , corregirlo y elevarlo como Push Request al repositorio de origen :
-*/
 
 (function(){
     "use strict"
@@ -43,7 +54,37 @@ let miembros = { pedro : 35 , ana : 18 , carlos : 43 , juan : 21 , maria : 29 , 
     })
     console.log(res) // [2,3]
 })()
+*/
 
+(function(){
+    "use strict"
+
+    let x = 1;
+	let arr = []
+    let y = 2
+    arr.push(x,y)
+    let res = [];
+	arr.forEach((n,indice)=>{
+        console.log(`El numero en el indice ${indice} es : ${n}`);
+		res.push(n+1);
+    })
+    console.log(res) // [2,3]
+})()
 /**
- * 7) Crear un modulo .js que contenga una variable llamada base cuyo valor es el número 2 y tres funciones, las cual va a exportar cada vez que se requiera el archivo llamadas multiplicar , cambiarBase y consultarBase. La función multiplicar toma un valor como input de tipo Number o String y lo multiplica por el valor de la variable base. La función cambiarBase modifica el valor de la variable base el cual se mantiene para las próximas ejecuciones y consultarBase retorna el valor actual de la variable base
- */
+ * 7) Crear un modulo .js que contenga una variable llamada base cuyo valor es el número 2 y tres funciones,
+ las cual va a exportar cada vez que se requiera el archivo llamadas multiplicar , cambiarBase y consultarBase.
+ La función multiplicar toma un valor como input de tipo Number o String y lo multiplica por el valor de la variable base.
+ La función cambiarBase modifica el valor de la variable base el cual se mantiene para las próximas ejecuciones y
+ consultarBase retorna el valor actual de la variable base */
+ var base = 2
+ module.export {
+ 	multiplicar: function(i) {
+ 		return i * base;
+ 	},
+ 	cambiarBase: function(b) {
+ 		base = b;
+ 	},
+ 	consultarBase: function() {
+ 		return base;
+ 	}
+ }
